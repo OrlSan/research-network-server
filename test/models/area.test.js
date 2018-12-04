00f5e69af4a544dad9d586b75a0cff32db3a3c69
Kimberly Becerril Fuentes <kymy@ciencias.unam.mx>
2018-12-04 15:08:14 -0600

    Add create and find area test

--- test/models/area.test.js
+++ test/models/area.test.js
@@ -1,26 +1,26 @@
 var assert = require('assert');
 const should = require('should');
-describe('\n\n________________________AREA__________________', () => {
- 
-    beforeEach(() => {
-        return factory.createMany('area', 10).
-        then(areas => {
-          console.log("** Created 10 areas");
-        })
-        .catch(err => {
-          console.log("Error", err);
-        });
-    });
-    describe('Find all areas', () => {
-      it('should find all areas', () => {
-        return Area.findAll({}).then(areas => {
-          areas.should.be.an.Object();
-          areas.should.be.not.empty();
-          areas.forEach(result => {
-            let area = result.toJSON();
-            area.should.have.keys('name', 'image_url');
-          });
-        });
+describe('\n\n________________________AREA________________________', () => {
+  describe('\n--------------Validations on creation--------------\n', () => {
+    let area;
+    it('should create valid area with required properties', () => {
+      return factory.create('area')
+      .then(areaCreated => {
+        area = areaCreated;
+        areaCreated.should.be.not.empty();
+        areaCreated.should.be.an.Object();
+        areaCreated.should.have.property('id');
+        areaCreated.should.have.property('name');
+        areaCreated.should.have.property('image_url');
       });
     });
+    it('should find area created', () => {
+      area = area.dataValues;
+      return Area.findOne({ where: { id: area.id }}).then(areaFinded => {
+        areaFinded.should.be.an.Object();
+        areaFinded.should.be.not.empty();
+        areaFinded.should.have.property('name', area.name);
+      });
+    });
+  });
 });

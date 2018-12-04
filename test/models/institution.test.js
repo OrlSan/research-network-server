c31083ad81522299de37fe7c5fe99225c863d396
Kimberly Becerril Fuentes <kymy@ciencias.unam.mx>
2018-12-04 15:06:20 -0600

    Add create and find institution test

--- test/models/institution.test.js
+++ test/models/institution.test.js
@@ -1,26 +1,30 @@
 var assert = require('assert');
 const should = require('should');
-describe('\n\n________________________INSTITUTION__________________', () => {
- 
-    beforeEach(() => {
-        return factory.createMany('institution', 10).
-        then(users => {
-          console.log("** Created 10 institutions");
-        })
-        .catch(err => {
-          console.log("Error", err);
-        });
-    });
-    describe('Find all institutions', () => {
-      it('should find all institutions', () => {
-        return Institution.findAll({}).then(results => {
-          results.should.be.an.Object();
-          results.should.be.not.empty();
-          results.forEach(result => {
-            let institution = result.toJSON();
-            institution.should.have.keys('name', 'faculty');
-          });
-        });
+describe('\n\n________________________INSTITUTION________________________', () => {
+  describe('\n--------------Validations on creation--------------\n', () => {
+    let institution;
+    it('should create valid institution with required properties', () => {
+      return factory.create('institution')
+      .then(institutionCreated => {
+        institution = institutionCreated;
+        institutionCreated.should.be.not.empty();
+        institutionCreated.should.be.an.Object();
+        institutionCreated.should.have.property('id');
+        institutionCreated.should.have.property('name');
+        institutionCreated.should.have.property('faculty');
+        institutionCreated.should.have.property('country');
+        institutionCreated.should.have.property('state');
+        institutionCreated.should.have.property('email');
+        institutionCreated.should.have.property('address');
       });
     });
+    it('should find institution created', () => {
+      institution = institution.dataValues;
+      return Institution.findOne({ where: { id: institution.id }}).then(institutionFinded => {
+        institutionFinded.should.be.an.Object();
+        institutionFinded.should.be.not.empty();
+        institutionFinded.should.have.property('name', institution.name);
+      });
+    });
+  });
 });

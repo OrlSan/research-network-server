b8ac0fdc4a7a5e84a22e4dc57e303545ea1d7649
Kimberly Becerril Fuentes <kymy@ciencias.unam.mx>
2018-12-04 15:14:57 -0600

    Add create, find and association publication test

--- test/models/publication.test.js
+++ test/models/publication.test.js
@@ -1,26 +1,53 @@
 var assert = require('assert');
 const should = require('should');
-describe('\n\n________________________PUBLICATION__________________', () => {
- 
-    beforeEach(() => {
-        return factory.createMany('publication', 10).
-        then(publications => {
-          console.log("** Created 10 publications");
-        })
-        .catch(err => {
-          console.log("Error", err);
-        });
+describe('\n\n________________________PUBLICATION________________________', () => {
+  describe('\n--------------Validations on creation--------------\n', () => {
+    let publication;
+    it('should create valid publication with required properties', () => {
+      return factory.create('publication')
+      .then(publicationCreated => {
+        publication = publicationCreated;
+        publicationCreated.should.be.not.empty();
+        publicationCreated.should.be.an.Object();
+        publicationCreated.should.have.property('id');
+        publicationCreated.should.have.property('title');
+        publicationCreated.should.have.property('type');
+        publicationCreated.should.have.property('is_public');
+        publicationCreated.should.have.property('abstract');
+      });
     });
-    describe('Find all publications', () => {
-      it('should find all publications', () => {
-        return Publication.findAll({}).then(results => {
-          results.should.be.an.Object();
-          results.should.be.not.empty();
-          results.forEach(result => {
-            let institution = result.toJSON();
-            institution.should.have.keys('title', 'type');
-          });
+    it('should find publication created', () => {
+      publication = publication.dataValues;
+      return Publication.findOne({ where: { id: publication.id }}).then(publicationFinded => {
+        publicationFinded.should.be.an.Object();
+        publicationFinded.should.be.not.empty();
+        publicationFinded.should.have.property('title', publication.title);
+      });
+    });
+  });
+
+  describe('\n--------------Validations on associations--------------\n', () => {
+    var area;
+    var publication;
+    before(() => {
+      return Promise.all([
+        factory.create('area'),
+        factory.create('publication'),
+      ]).then(result => {
+        area = result[0];
+        publication = result[1];
+        return publication.setArea(area);
+      });
+    });
+    describe('Find publication with specific area', () => {
+      it('should find publication with specific associated area', () => {
+        return Publication.findOne({ where: { id: publication.dataValues.id }}).then(publication => {
+          publication = publication.dataValues;
+          publication.should.be.an.Object();
+          publication.should.be.not.empty();
+          publication.should.have.property('AreaId', area.dataValues.id);
         });
       });
     });
+  });
 });
